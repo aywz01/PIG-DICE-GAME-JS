f9fb48074d4a87d67a406cbf2556d8c9d30efb3c
Ayşe Yavuz <ayseyavuz702@gmail.com>
2024-02-04 22:34:32 +0300

    created init function for new game

--- script.js
+++ script.js
@@ -1,30 +1,53 @@
 "use strict";
 //selecting elements
-const player1El = document.querySelector(".player-1");
-const player2El = document.querySelector(".player-2");
+const player1El = document.querySelector(".player--1");
+const player2El = document.querySelector(".player--2");
 //these are the same, getElementById works faster
-const score1El = document.getElementById("score-1");
-const score2El = document.querySelector("#score-2");
+const score1El = document.getElementById("score--1");
+const score2El = document.querySelector("#score--2");
 //selecting dice
 const diceEl = document.querySelector(".dice");
 //selecting current elements
-const current1El = document.getElementById("current-1");
-const current2El = document.getElementById("current-2");
+const current1El = document.getElementById("current--1");
+const current2El = document.getElementById("current--2");
 //selecting buttons
-const btnNewEl = document.querySelector(".btn-new");
-const btnRollEl = document.querySelector(".btn-roll");
-const btnHoldEl = document.querySelector(".btn-hold");
+const btnNewEl = document.querySelector(".btn--new");
+const btnRollEl = document.querySelector(".btn--roll");
+const btnHoldEl = document.querySelector(".btn--hold");
 
-//First apperance of the page
-//Empty all the scores,dice and values
-score1El.textContent = 0;
-score2El.textContent = 0;
-diceEl.classList.add("hidden");
-current1El.textContent = 0;
-current2El.textContent = 0;
-//declare score for hold the value
+//declaring variables
 let score1 = 0;
 let currentVal = 0;
+
+const init = function () {
+  //Empty all the scores,dice and values
+  //also First apperance of the page
+
+  score1 = 0;
+  currentVal = 0;
+  // scores = [0, 0];
+  // currentScore = 0;
+  // activePlayer = 0;
+  // playing = true;
+
+  //make all the elements 0
+  score1El.textContent = 0;
+  score2El.textContent = 0;
+  current1El.textContent = 0;
+  current2El.textContent = 0;
+
+  //remove dice img
+  diceEl.classList.add("hidden");
+
+  //turn bg to blue and fonts to normal
+  // player1El.classList.remove("player--winner");
+  // player2El.classList.remove("player--winner");
+
+  //Select player1 as active player
+  // player1El.classList.add("player--active");
+  // player2El.classList.remove("player--active");
+};
+init();
 
 //Rolling the dice
 btnRollEl.addEventListener("click", function () {
@@ -55,16 +78,8 @@ btnHoldEl.addEventListener("click", function () {
   current1El.textContent = 0;
   currentVal = 0;
   score1 = 0;
+  //add the condition where score is bigger equal to 100
 });
 
 //new game button
-btnNewEl.addEventListener("click", function () {
-  //First apperance of the page
-  //Empty all the scores,dice and values
-  score1El.textContent = 0;
-  score2El.textContent = 0;
-  diceEl.classList.add("hidden");
-  current1El.textContent = 0;
-  current2El.textContent = 0;
-  //background also should turn to blue if someone win and it was black
-});
+btnNewEl.addEventListener("click", init);
