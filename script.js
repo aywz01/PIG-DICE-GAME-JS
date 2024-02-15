66650daedc769a2f458864764a71f5d095214c57
Ayşe Yavuz <ayseyavuz702@gmail.com>
2024-02-15 21:37:27 +0300

    player0

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 "use strict";
 //selecting elements
-const player1El = document.querySelector(".player--1");
-const player2El = document.querySelector(".player--2");
+const player1El = document.querySelector(".player--0");
+const player2El = document.querySelector(".player--1");
 //these are the same, getElementById works faster
-const score1El = document.getElementById("score--1");
-const score2El = document.querySelector("#score--2");
+const score1El = document.getElementById("score--0");
+const score2El = document.querySelector("#score--1");
 //selecting dice
 const diceEl = document.querySelector(".dice");
 //selecting current elements
-const current1El = document.getElementById("current--1");
-const current2El = document.getElementById("current--2");
+const current1El = document.getElementById("current--0");
+const current2El = document.getElementById("current--1");
 //selecting buttons
 const btnNewEl = document.querySelector(".btn--new");
 const btnRollEl = document.querySelector(".btn--roll");
@@ -17,14 +17,18 @@ const btnHoldEl = document.querySelector(".btn--hold");
 
 //declaring variables
 let score1 = 0;
-let currentVal = 0;
+let currentScore = 0;
+let activePlayer = 0;
+//storing players scores in array
+const scores = [0, 0];
+//because arrays index is 0, players will be player0 and player1
 
 const init = function () {
   //Empty all the scores,dice and values
   //also First apperance of the page
 
   score1 = 0;
-  currentVal = 0;
+  currentScore = 0;
   // scores = [0, 0];
   // currentScore = 0;
   // activePlayer = 0;
@@ -72,11 +76,11 @@ btnRollEl.addEventListener("click", function () {
 //hold the score
 btnHoldEl.addEventListener("click", function () {
   score1 = parseInt(score1El.textContent); //turning the text content into number to be able to do summation
-  currentVal = parseInt(current1El.textContent);
-  score1 = score1 + currentVal;
+  currentScore = parseInt(current1El.textContent);
+  score1 = score1 + currentScore;
   score1El.textContent = score1;
   current1El.textContent = 0;
-  currentVal = 0;
+  currentScore = 0;
   score1 = 0;
   //after pressing hold the current player will change
 
